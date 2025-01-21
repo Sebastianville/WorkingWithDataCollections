499a431d92bc34accdeaa19608e1e55b70ccfc00
Sebastianville <sebastian.villegas2623@gmail.com>
2025-01-21 16:17:59 -0500

    part 3

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ for(let i=0; i <= csv.length;i++)
     if(curChar==="," ||curChar==="\n" )
     {
         row.push(cell);
-        cell= " ";
+        cell= " ".trim()
     }
     if(curChar==="\n")
     {
@@ -20,4 +20,34 @@ for(let i=0; i <= csv.length;i++)
             cell += curChar;
         }
 }
-console.log(Results)
+// console.log(Results)
+
+//part 3 
+
+let newCSV = [
+    ["ID", "Name", "Occupation", "Age"],
+    ["42", "Bruce", "Knight", "41"],
+    ["57", "Bob", "Fry Cook", "19"],
+    ["63", "Blaine", "Quiz Master", "58"],
+    ["98", "Bill", "Doctor’s Assistant", "26"],
+  ];
+
+  let changedCSV = [];
+
+
+let header = newCSV[0]
+// console.log(header)
+
+
+for ( let i= 1; i < newCSV.length; i ++) {
+    // console.log(newCSV[i])
+    let row = newCSV[i]
+    let objects = {}
+    
+
+    for (let j = 0; j < row.length; j ++) {
+        //this is looping thorugh each array and getting all of the elements individually
+        console.log(objects[header[j].toLocaleLowerCase()] = row[j])
+    }
+    changedCSV.push(objects)
+}
